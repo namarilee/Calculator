ad380d81991ba764cb1f3ad6c427df5230b79ca6
Marina Lee <leemarina@gmail.com>
2020-12-27 00:44:22 -0800

    rounded corners

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,9 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var percentButton: UIButton!
+    @IBOutlet weak var plusMinus: UIButton!
+    @IBOutlet weak var AC: UIButton!
+    @IBOutlet weak var divide: UIButton!
+    @IBOutlet weak var seven: UIButton!
+    @IBOutlet weak var eight: UIButton!
+    @IBOutlet weak var nine: UIButton!
+    @IBOutlet weak var times: UIButton!
+    @IBOutlet weak var four: UIButton!
+    @IBOutlet weak var five: UIButton!
+    @IBOutlet weak var six: UIButton!
+    @IBOutlet weak var minus: UIButton!
+    @IBOutlet weak var one: UIButton!
+    @IBOutlet weak var two: UIButton!
+    @IBOutlet weak var three: UIButton!
+    @IBOutlet weak var plus: UIButton!
+    @IBOutlet weak var zero: UIButton!
+    @IBOutlet weak var dot: UIButton!
+    @IBOutlet weak var equals: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        percentButton.layer.cornerRadius = 15
+        plusMinus.layer.cornerRadius = 15
+        AC.layer.cornerRadius = 15
+        divide.layer.cornerRadius = 15
+        seven.layer.cornerRadius = 15
+        eight.layer.cornerRadius = 15
+        nine.layer.cornerRadius = 15
+        times.layer.cornerRadius = 15
+        four.layer.cornerRadius = 15
+        five.layer.cornerRadius = 15
+        six.layer.cornerRadius = 15
+        minus.layer.cornerRadius = 15
+        one.layer.cornerRadius = 15
+        two.layer.cornerRadius = 15
+        three.layer.cornerRadius = 15
+        plus.layer.cornerRadius = 15
+        zero.layer.cornerRadius = 15
+        dot.layer.cornerRadius = 15
+        equals.layer.cornerRadius = 15
+
     }
 
 
